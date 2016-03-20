71c40631caa913b5aa8d18418c4d5e8f92358604
Nicholas Naudé <nicnaude@gmail.com>
2016-03-20 10:35:48 -0700

    First commit of 20 March

---
+++ Giddy/RootTableVC.swift
@@ -0,0 +1,162 @@
+//
+//  RootTableVC.swift
+//  Giddy
+//
+//  Created by Nicholas Naudé on 19/03/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class RootTableVC: UITableViewController {
+    
+    var toDos = [CKRecord]()
+    var refresh:UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refresh = UIRefreshControl()
+        refresh.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refresh.addTarget(self, action: "loadData", forControlEvents: .ValueChanged)
+        self.tableView.addSubview(refresh)
+        
+        loadData()
+    }
+    
+    func loadData() {
+        toDos = [CKRecord]()
+        
+        let publicData = CKContainer.defaultContainer().publicCloudDatabase
+        let query = CKQuery (recordType: "toDo", predicate: NSPredicate(format: "TRUEPREDICATE", argumentArray: nil))
+        
+        query.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        publicData.performQuery(query, inZoneWithID: nil) { (results:[CKRecord]?, error:NSError?) -> Void in
+            
+            if let toDos = results {
+                self.toDos = toDos
+                dispatch_async(dispatch_get_main_queue(), {() -> Void in
+                    self.tableView.reloadData()
+                    self.refresh.endRefreshing()
+                })
+            }
+        }
+    }
+    
+    @IBAction func sendToDo(sender: AnyObject) {
+        let alert = UIAlertController(title: "Get it done", message: "Add a new todo", preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField:UITextField) -> Void in
+            textField.placeholder = "Buy milk"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Add", style: .Default, handler: { (action:UIAlertAction) -> Void in
+            let textField = alert.textFields?.first
+            if textField!.text != "" {
+                let newToDo = CKRecord(recordType: "ToDo")
+                newToDo["content"] = textField!.text
+                
+                let publicData = CKContainer.defaultContainer().publicCloudDatabase
+                publicData.saveRecord(newToDo, completionHandler: { (record:CKRecord?, error:NSError?) -> Void in
+                    
+                    if error == nil {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.tableView.beginUpdates()
+                            self.toDos.insert(newToDo, atIndex: 0)
+                            let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+                            self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Top)
+                            self.tableView.endUpdates()
+                        })
+                    } else {
+                        print("Error!")
+                    }
+                })
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    //    func errorAlert () {
+    //    let alert = UIAlertController(title: "Not saved", message: "An error occured. Please try again later", preferredStyle: .Alert)
+    //    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        if toDos.count == 0 {
+            return cell
+        }
+        
+        let toDo = toDos[indexPath.row]
+        
+        if let toDosContent = toDo["content"] as? String {
+            let dateFormat = NSDateFormatter()
+            dateFormat.dateFormat = "MM/dd/yyyy"
+            let dateString = dateFormat.stringFromDate(toDo.creationDate!)
+            
+            cell.textLabel?.text = toDosContent
+            cell.detailTextLabel?.text = dateString
+        }
+        
+        return cell
+    }
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
