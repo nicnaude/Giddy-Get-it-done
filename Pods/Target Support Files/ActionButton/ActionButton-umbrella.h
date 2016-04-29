0e3838b0807fb1fc358bd795e8ec9c0ca21e1acd
Nicholas Naudé <nicnaude@gmail.com>
2016-04-29 09:36:23 +0100

    Removed podfile

--- Pods/Target Support Files/ActionButton/ActionButton-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double ActionButtonVersionNumber;
-FOUNDATION_EXPORT const unsigned char ActionButtonVersionString[];
-
--- Pods/Target Support Files/Pods-Giddy/Pods-Giddy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Giddy : NSObject
-@end
-@implementation PodsDummy_Pods_Giddy
-@end
